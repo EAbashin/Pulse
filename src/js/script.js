e26a1b6bee0b1ffc696736d591a1f6226d81118d
Evgeniy Abashin <aevgen.001@gmail.com>
2022-03-06 14:46:47 +0300

    v1.6

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,46 @@
+// slick slider
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1000,
         adaptiveHeight: false,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/slide/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/slide/right.svg"></button>',
+        prevArrow: '<button type="button" class="prev"><img src="img/slide/left.svg"></button>',
+        nextArrow: '<button type="button" class="next"><img src="img/slide/right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
                 settings: {
-                dots: true,
+                dots: false,
                 arrows: false
                 }
             }
         ]
     });
 });
+// tiny slider
+// const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     // slideBy: 'page',
+//     autoplay: false,
+//     controls: false,
+//     nav: false,
+//     responsive: {
+//         320: {
+//             gutter: 10,
+//             touch: true,
+//             mouseDrag: true
+//         },
+//         900: {
+//             touch: true,
+//             gutter: 20,
+//             mouseDrag: true
+//         }
+//     }
+//   });
+// slick slider - arrow connection
+// document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo('prev');
+// });
+// document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo('next');
+// });
