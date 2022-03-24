c3c896c760631b6ed36f30f9604785119b8f1987
Evgeniy Abashin <aevgen.001@gmail.com>
2022-03-24 11:45:10 +0300

    v1.14

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,22 @@ $(document).ready(function(){
     valideForm('#order form');
 
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut('slow');
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
 });
 // tiny slider
 // const slider = tns({
